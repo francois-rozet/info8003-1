1098541ea34bfcb53659365b5d0de27fe0a18338
François Rozet <francois.rozet@outlook.com>
2021-02-19 19:58:26 +0100

    Assignment 2.1 Implementation of the Domain

---
+++ assignments/2/python/section1.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python
+
+from domain import *
+
+
+# 1.a Rule-based policies
+
+def uniform(x: State) -> Action:
+    '''Random uniform policy'''
+
+    return random.choice(U)
+
+
+def notsofastandfurious(x: State) -> Action:
+    '''Full-throttle policy'''
+
+    return U[1]
+
+
+def stepback(x: State) -> Action:
+    '''Sometimes-you-have-to-take-a-step-back-to-move-forward policy'''
+
+    if -0.5 < x[0] < 0 and -1.5 < x[1] < 0:
+        return U[0]
+    else:
+        return U[1]
+
+
+# 1.b Apply
+
+if __name__ == '__main__':
+    h = simulate(stepback)
+
+    print(*h, sep='\n')
