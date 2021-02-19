b577218028593749bc4077422f1c2837dab94cec
François Rozet <francois.rozet@outlook.com>
2021-02-19 10:26:45 +0100

    Assignment 2. Display

---
+++ assignments/2/python/display.py
@@ -0,0 +1,192 @@
+#!usr/bin/env python
+
+import imageio
+import pygame as pg
+import time
+
+from functools import lru_cache as cache
+from math import atan2, degrees
+
+from domain import *
+
+
+# Globals
+
+SIZE = 400
+
+PINE = '../resources/pine_tree.png'
+CAR = '../resources/car.png'
+
+
+# Classes
+
+class PyGameDisplay:
+    def __init__(self):
+        self.screen = None
+
+    def __enter__(self):
+        self.screen = pg.display.set_mode((SIZE, SIZE))
+
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pg.display.quit()
+
+    def __bool__(self):
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                return False
+
+        return True
+
+    def __call__(self, surf: pg.Surface):
+        self.screen.blit(surf, (0, 0))
+        pg.display.update()
+
+
+# Functions
+
+def x2p(x: int) -> float:
+    return 2 * (x / SIZE) - 1
+
+
+def p2x(p: float) -> int:
+    return round(SIZE * (1 + p) / 2)
+
+
+def h2y(h: float) -> int:
+    return round(SIZE * (1 - h) / 2)
+
+
+@cache()
+def load(filename: str) -> pg.Surface:
+    img = pg.image.load(filename)
+    img.convert_alpha()
+
+    return img
+
+
+@cache()
+def background() -> pg.Surface:
+    surf = pg.Surface((SIZE, SIZE))
+
+    # Sky, hill & road
+
+    for x in range(SIZE):
+        z = h2y(hill(x2p(x)))
+
+        for y in range(SIZE):
+            if y < z:
+                color = (64, 163, 191)  # sky
+            elif y > z:
+                color = (64, 191, 114)  # hill
+            else:
+                color = (0, 0, 0)  # road
+
+            surf.set_at((x, y), color)
+
+    # Trees
+
+    pine = load(PINE)
+    w, h = pine.get_rect().size
+
+    for p in [-0.5, 0.5]:
+        x, y = p2x(p), h2y(hill(p))
+        surf.blit(pine, (x - w // 2, y - h))
+
+    return surf
+
+
+def draw(p: float, s: float) -> pg.Surface:
+    surf = background().copy()
+
+    # Car
+
+    car = load(CAR)
+    w, h = car.get_rect().size
+
+    x, y = p2x(p), h2y(hill(p))
+    angle = degrees(atan2(hill_prime(p), 1))
+
+    car = pg.transform.rotate(car, angle)
+
+    surf.blit(car, car.get_rect(center=(x, y - h // 2)))
+
+    # Speedometer
+
+    location = (SIZE - 70, SIZE - 70)
+    width, height = 30, 50
+    thickness = 3
+
+    ## Line
+
+    rect = pg.Rect(location, (width, thickness))
+    surf.fill((0, 0, 0), rect)
+
+    ## Speed
+
+    norm = min(abs(s), 3) / 3
+    red = int(norm * 255)
+    green = 255 - red
+    height = int(norm * height)
+
+    rect = pg.Rect(location, (width, height))
+
+    if s < 0:
+        rect = rect.move(0, thickness)
+    else:
+        rect = rect.move(0, -height)
+
+    surf.fill((red, green, 0), rect)
+
+    return surf
+
+
+def gifwrite(frames: List[pg.Surface], filename: str):
+    with imageio.get_writer(filename, duration=TIME_STEP) as writer:
+        for frame in frames:
+            array = pg.surfarray.pixels3d(frame).swapaxes(0, 1)
+            writer.append_data(array)
+
+
+def animate(h: Trajectory, filename: str = None):
+    with PyGameDisplay() as screen:
+        frames = []
+
+        for l in h:
+            if not screen:
+                return
+
+            frame = draw(*l[0])
+
+            if filename is None:
+                screen(frame)
+                time.sleep(TIME_STEP)
+            else:
+                frames.append(frame)
+
+        if frames:
+            frame = draw(*l[-1])
+            frames.append(frame)
+            gifwrite(frames, filename)
+
+
+# Main
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', '--position', type=float, default=0.)
+    parser.add_argument('-s', '--speed', type=float, default=1.)
+
+    args = parser.parse_args()
+
+    p, s = args.position, args.speed
+
+    with PyGameDisplay() as screen:
+        screen(draw(p, s))
+
+        while screen:
+            pass
