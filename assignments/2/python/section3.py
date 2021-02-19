5500b4cc14de1c1889a3efc257d58f88b8bc9f3e
François Rozet <francois.rozet@outlook.com>
2021-02-19 19:59:54 +0100

    Assignment 2.3 Visualization

---
+++ assignments/2/python/section3.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+
+from display import animate
+from section1 import *
+
+
+# 3. Visualization
+
+if __name__ == '__main__':
+    h = simulate(stepback, x=(0, 0))
+
+    animate(h, filename='stepback.gif')
